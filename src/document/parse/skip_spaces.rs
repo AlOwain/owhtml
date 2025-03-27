f2713029f04835c3eda56dfb3cbee7c6bf3fddbd
Aziz AlOwain <az@alowain.dev>
2025-03-27 09:42:47 +0300

    Create some basic unit tests for `skip_spaces`
    This also adds a simple declarative macro to be able to quickly implement
    new unit tests for `skip_spaces`, this creates a few scenarios and covers
    a wide enough base to assume that `skip_spaces` has more or less the
    intended behavior.

--- src/document/parse/skip_spaces.rs
+++ src/document/parse/skip_spaces.rs
@@ -1,11 +1,45 @@
 use std::iter::Peekable;
 
-// TODO: This should be tested properly.
 pub(super) fn skip_spaces(iter: &mut Peekable<impl Iterator<Item = char>>) {
     while let Some(c) = iter.peek() {
         if !c.is_whitespace() {
             break;
         }
         iter.next();
+    }
+}
+
+mod test {
+    #![allow(unused_macros, unused_imports)]
+    use std::{iter::Peekable, str::Chars};
+
+    macro_rules! call_skip_spaces {
+        ($str: literal, $iterate_count: literal, $result: expr) => {
+            let mut i: Peekable<Chars> = $str.chars().peekable();
+            // TODO(nightly): Use Iterator::advance_by when stable.
+            let _ = i.nth($iterate_count - 1).unwrap();
+            super::skip_spaces(&mut i);
+            assert_eq!(i.peek(), $result);
+        };
+
+        ($str: literal, $result: expr) => {
+            let mut i: Peekable<Chars> = $str.chars().peekable();
+            super::skip_spaces(&mut i);
+            assert_eq!(i.peek(), $result);
+        };
+    }
+
+    #[test]
+    #[rustfmt::skip]
+    pub fn test_skip_spaces() {
+        call_skip_spaces!("",                    None);
+        call_skip_spaces!("O X",                 Some(&'O'));
+        call_skip_spaces!(" O X",                Some(&'O'));
+        call_skip_spaces!("  O X",               Some(&'O'));
+        call_skip_spaces!("\nO X",               Some(&'O'));
+        call_skip_spaces!(" \n\t\nO X",          Some(&'O'));
+        call_skip_spaces!("X  ",           1,    None);
+        call_skip_spaces!("X\n\t ",        1,    None);
+        call_skip_spaces!("X\n\tO",        3,    Some(&'O'));
     }
 }
